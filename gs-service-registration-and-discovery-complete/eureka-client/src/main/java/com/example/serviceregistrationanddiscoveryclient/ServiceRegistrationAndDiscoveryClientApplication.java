1aec8ced3e2680cf1ca2f83f1ddb6af68a2d9b85
unknown <pariwesh06>
2020-12-17 15:23:50 +0530

    ribbon demo

--- gs-service-registration-and-discovery-complete/eureka-client/src/main/java/com/example/serviceregistrationanddiscoveryclient/ServiceRegistrationAndDiscoveryClientApplication.java
+++ gs-service-registration-and-discovery-complete/eureka-client/src/main/java/com/example/serviceregistrationanddiscoveryclient/ServiceRegistrationAndDiscoveryClientApplication.java
@@ -6,13 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ServiceRegistrationAndDiscoveryClientApplication {
 
